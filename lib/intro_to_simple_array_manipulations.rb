6cb99263183e21e8c1247d312f5b3013d78cdbb4
Vivian Tran <viviantran94@gmail.com>
2019-10-19 22:00:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,6 @@ def using_push(array, next_color)
   array.push(next_color)
 end
 
-def using_unshift(array, new_neighbor)
+def using_unshift(array, new_neighborhood)
+  array.unshift(new_neighborhood)
+end
